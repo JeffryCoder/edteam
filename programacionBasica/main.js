2c5a37418825f5fe21886dcba8d4c6c6fd2cfc01
Jeffry Sánchez <jeffry@siglo21.local>
2023-01-16 15:11:42 -0400

    Agregando commi inicial

---
+++ programacionBasica/main.js
@@ -0,0 +1,9 @@
+alert("Programa funcionando!")
+
+let owner = prompt("Quien quieres que sea tu dueño?");
+
+for(i = 1; i < 10; i++){
+    console.log(i);
+}
+
+console.log(`El dueño de Cleudy es ${owner}`);
