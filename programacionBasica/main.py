a60b115e1d4669c7175a3fdec2daaf3402cc9734
jeffrycoder <jeffrycoder@gmail.com>
2023-01-17 12:35:39 -0400

    Comentanto y agregando array de frutas

--- programacionBasica/main.py
+++ programacionBasica/main.py
@@ -1,23 +1,23 @@
 # PROGRAMA (JUEGO) ADIVINA EL NUMERO
 
-numeroParaAdivinar = 13549
-numeroIntroducido = 0
+# numeroParaAdivinar = 13549
+# numeroIntroducido = 0
 
-print("Hola, bienvenido al juego 'Adivinalo'.")
-print("Tienes que adivinar un numero que estoy pensando")
-print("Comencemos!")
-print("")
+# print("Hola, bienvenido al juego 'Adivinalo'.")
+# print("Tienes que adivinar un numero que estoy pensando")
+# print("Comencemos!")
+# print("")
 
-while numeroIntroducido != numeroParaAdivinar:
+# while numeroIntroducido != numeroParaAdivinar:
 
-    numeroIntroducido = int(input("¿Cuál es el numero?: "))
+#     numeroIntroducido = int(input("¿Cuál es el numero?: "))
 
-    if numeroIntroducido > numeroParaAdivinar:
-        print("Digita un numero menor: ")
-    elif numeroIntroducido < numeroParaAdivinar:
-        print("Digita un numero mayor: ")
-    else:
-        print("Felicitaciones, ¡Adivinaste el numero!")
+#     if numeroIntroducido > numeroParaAdivinar:
+#         print("Digita un numero menor: ")
+#     elif numeroIntroducido < numeroParaAdivinar:
+#         print("Digita un numero mayor: ")
+#     else:
+#         print("Felicitaciones, ¡Adivinaste el numero!")
    
 
 
@@ -25,5 +25,13 @@ while numeroIntroducido != numeroParaAdivinar:
 
 # numerosEnteros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-
 # Recordar mejorar esta tarea, agregando un juego para adivinar un numero y un tablero de posicione spara las personas que adivinan un numero con la menor cantidad de intentos posibles
+
+
+
+#Ejercicios arreglos
+
+
+frutas = ["Mango", "Guayaba", "Guineo"]
+
+print(frutas)
