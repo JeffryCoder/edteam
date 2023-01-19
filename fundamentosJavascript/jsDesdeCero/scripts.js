8b746dcf3286f7b53b54918f5b3703cc404bc8f0
jeffrycoder <jeffrycoder@gmail.com>
2023-01-19 14:39:42 -0400

    Agregando practica con arrays y objetos

---
+++ fundamentosJavascript/jsDesdeCero/scripts.js
@@ -0,0 +1,27 @@
+
+
+/**
+ * Tipos de datos - Colección de datos
+ */
+
+/**
+ * Objetos:
+ * 
+ * Coleciones de datos (informacion)
+ * que tienen un identificador y un valor
+ */
+
+// Array (Arreglo)
+
+["Cleudy", "Jeffry", "Nelson", "Vladimir", "Anderson", "Diego"]
+
+console.log( ["Cleudy", "Jeffry", "Nelson", "Vladimir", "Anderson", "Diego"])
+
+
+// Objetos JAVASCRIPT
+
+console.log({
+    nombre: "Jeffry",
+    apellido: "Sanchez"
+})
+
