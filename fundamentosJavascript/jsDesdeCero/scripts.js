9a166f5d61fb6016baf98efd880f520f2d23fc30
jeffrycoder <jeffrycoder@gmail.com>
2023-01-23 12:21:44 -0400

    Agregando ejemplo de typecoertion

--- fundamentosJavascript/jsDesdeCero/scripts.js
+++ fundamentosJavascript/jsDesdeCero/scripts.js
@@ -11,13 +11,23 @@
 
 // Array (Arreglo)
 
-["Cleudy", "Jeffry", "Nelson", "Vladimir", "Anderson", "Diego"]
+// ["Cleudy", "Jeffry", "Nelson", "Vladimir", "Anderson", "Diego"]
 
-console.log( ["Cleudy", "Jeffry", "Nelson", "Vladimir", "Anderson", "Diego"])
+// console.log( ["Cleudy", "Jeffry", "Nelson", "Vladimir", "Anderson", "Diego"])
 
-// Objetos JAVASCRIPT
-console.log({
-    nombre: "Jeffry",
-    apellido: "Sanchez"
-})
+// // Objetos JAVASCRIPT
+// console.log({
+//     nombre: "Jeffry",
+//     apellido: "Sanchez"
+// })
+
+// TYPE COERTION JAVASCRIPT
+
+let a = 521
+let b = "422"
+
+let respuesta1 = (a + b)
+let respuesta2 = (a - b)
+
+console.log(`El resultado de "a" es: ${respuesta1} y el resultado de "b" es ${respuesta2}`)
 
