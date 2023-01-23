4a0683ab0bfd09138a2a0da4e843cd584b2d6649
jeffrycoder <jeffrycoder@gmail.com>
2023-01-23 12:45:46 -0400

    Tarea sobre mayoria de edad realizada

--- fundamentosJavascript/jsDesdeCero/scripts.js
+++ fundamentosJavascript/jsDesdeCero/scripts.js
@@ -23,13 +23,32 @@
 
 // TYPE COERTION JAVASCRIPT
 
-let a = 521
-let b = "422"
+// let a = 521
+// let b = "422"
 
-let respuesta1 = (a + b)
-let respuesta2 = (a - b)
+// let respuesta1 = (a + b)
+// let respuesta2 = (a - b)
 
-console.log(`El resultado de "a" es: ${respuesta1} y el resultado de "b" es ${respuesta2}`)
+// console.log(`El resultado de "a" es: ${respuesta1} y el resultado de "b" es ${respuesta2}`)
 
 
 // Recordar investigar sobre TypeCoertion
+
+/**
+ * CONDICIONALES
+ */
+
+/**
+ *  Primer problema. Invitación a una fiesta
+ *  Si eres mayor de edad y menor de 65 años puedes venir a la fiesta
+ */
+let nombre = prompt("¿Cuál es tu nombre?")
+let edad = prompt("¿Cuál es tu edad?")
+
+if (edad >= 18 && edad < 65) {
+    console.log("Puedes venir a la fiesta")
+    console.log(`Te esperamos, ${nombre}.`)
+} else {
+    console.log("No puedes venir a la fiesta, no cumples los requisitos de edad")
+    console.log(`Lo sentimos, ${nombre}.`)
+}
