8c3abaadf1bf47078a85d607a44a40f83fd47656
jeffrycoder <jeffrycoder@gmail.com>
2023-02-10 14:11:17 -0400

    Comentando ejercicio funcion saludar

--- fundamentosJavascript/claseFunciones/main.js
+++ fundamentosJavascript/claseFunciones/main.js
@@ -1,11 +1,20 @@
 /**
- * FUNCIONES
+ * FUNCIONES - Parámetros
  */
 
-// Declaración de mi funcion saludar
-function saludar(){
-    console.log("Hola, esto es un saludo")
-}
+// // Declaración de mi funcion saludar
+// function saludar(nombreUsuario){
+//     console.log(typeof nombreUsuario)
+//     if (typeof nombreUsuario === "string"){
+//         console.log(`Hola, ${nombreUsuario}, bienvenido al sistema!`)
+//     } else {
+//         console.log("Esto no es un nombre valido")
+//     }
+//     console.log("Hola, " + nombreUsuario + ", bienvenido al sistema!")
+//     console.log(typeof nombreUsuario)
+// }
 
-// LLamado a la funcion saludar
-saludar()
+// // LLamado a la funcion saludar
+// saludar(15);
+
+
