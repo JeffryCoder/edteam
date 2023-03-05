cb4656b4ad7a8df5ec756675ea1666c3541177f8
jeffrycoder <jeffrycoder@gmail.com>
2023-03-05 11:58:03 -0400

    Apregando modificaciones a la tarea

---
+++ fundamentosJavascript/tareas/dele3ADoor.js
@@ -0,0 +1,12 @@
+var nombreUsuario = prompt("Hola, bienvenido al sistema, introduzca su nombre de usuario para continuar: ");
+
+console.log("Bienvenido al sistema Dele-3A-Door");
+console.log("Para empezar vamos a deletrear su nombre");
+
+for ( let i = 0; i < nombreUsuario.length ; i++){
+    console.log(nombreUsuario[i]);
+}
+
+
+
+
