b71f7e98996604f73e421b4eda156ab62a904928
jeffrycoder <jeffrycoder@gmail.com>
2023-03-22 10:58:41 -0400

    Se agregaron modificaciones de ejemplo

--- fundamentosJavascript/2_coleccionesDeDatos/main.js
+++ fundamentosJavascript/2_coleccionesDeDatos/main.js
@@ -12,3 +12,18 @@ let amigos = ["Juan", "Pedro", "Pablo", "Luis", "Ricardo"];
 
 console.log(amigos);
 
+
+
+amigos.push("Cleudy");    // Agrega un elemento al final del array
+
+console.log(amigos)
+
+
+
+
+/**
+ * APUNTES DE CLASE
+ * 
+ * Si guardamos un metodo push de un array en una variable y la imprimimos, obtendremos el numero de elementos que tiene el array.
+ * 
+ */
