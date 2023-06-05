358938d9e85d6d6cd15ec28ad4977b14168d6893
jeffrycoder <jeffrycoder@gmail.com>
2023-06-05 14:52:23 -0400

    Commit inicial

---
+++ jsDesdeCero/6.1 - Metodos de Array.js
@@ -0,0 +1,34 @@
+alert("Funciona!");
+
+// 1. Metodos de Array
+
+/**
+ * 
+ *  ARRAYS
+ * 
+ * * */
+
+// Array declarado
+let amigos = ["Pedro", "Juan", "Pepe", "Luis", "Carlos"];
+
+
+// Metodo .push() - Agrega un elemento al final del array
+
+
+// Si guardamos lo que retorna el metodo .push, obtendremos la cantidad de elementos del array.
+
+
+/**
+ * 
+ *   METODOS QUE NO MODIFICAN EL ARRAY
+ * * */
+
+
+/***
+ * Metodo .pop() - Elimina el ultimo elemento del array
+ */
+
+
+/***
+ * Metodo slice() - Devuelve un nuevo array con los elementos seleccionados "parte a la mitad"
+ */
---
+++ jsDesdeCero/6.3 - Foreach.js
@@ -0,0 +1,11 @@
+/***
+ *  METODO FOREACH DE LOS ARRAYS
+ */
+
+let amigos = ["Pedro", "Juan", "Pepe", "Luis", "Carlos"];
+
+
+amigos.forEach(function(amigo){
+    
+    console.log(amigo);
+})
