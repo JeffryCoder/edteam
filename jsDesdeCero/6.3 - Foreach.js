0957fa348bd4f9d7e92221a897420561c1992fca
jeffrycoder <jeffrycoder@gmail.com>
2023-06-05 15:36:54 -0400

    Se agregaron apuntes

--- jsDesdeCero/6.3 - Foreach.js
+++ jsDesdeCero/6.3 - Foreach.js
@@ -4,8 +4,18 @@
 
 let amigos = ["Pedro", "Juan", "Pepe", "Luis", "Carlos"];
 
+amigos.forEach(amigo =>
+    console.log(amigo)
+)
 
-amigos.forEach(function(amigo){
-    
-    console.log(amigo);
-})
+/***
+ * METODO.FOREACH() - Recorre el array y ejecuta una funcion por cada elemento del array Y NO RETORNA NADA
+ */
+
+/***
+ * METODO .map - Recorre el array y ejecuta una funcion por cada elemento del array Y RETORNA UN NUEVO ARRAY
+ */
+
+/***
+ * Recordar revisar la documentacion en la mozila developer network
+ */
