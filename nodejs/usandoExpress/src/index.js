c215c888f71f51e8cdaec88ad0ead37c0863ac4c
jeffrycoder <jeffrycoder@gmail.com>
2023-08-18 12:53:32 -0400

    Agregando mis archivos estaticos

--- nodejs/usandoExpress/src/index.js
+++ nodejs/usandoExpress/src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 const user = require('./routes/users');
 
 // Middlewares
-const loggedMiddleware = require('./middlewares/logged')
-app.use(loggedMiddleware.isLogged);
+//const loggedMiddleware = require('./middlewares/logged')
+//app.use(loggedMiddleware.isLogged);
 
-app.use(express.static(__dirname+'./public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas
 
