6f6bfaf1c2d038389a980efc9b55ad32ba2714e3
jeffrycoder <jeffrycoder@gmail.com>
2023-08-18 15:46:41 -0400

    Envio de datos por x-www-form-urlencoded

--- nodejs/usandoExpress/src/index.js
+++ nodejs/usandoExpress/src/index.js
@@ -17,6 +17,8 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({extended:false}))
+
 // Rutas
 
 app.get('/', (req, res)=> {
