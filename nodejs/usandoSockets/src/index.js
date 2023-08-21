198da8d6a5bf6f6e32c1813b0e904f05dbcefc28
jeffrycoder <jeffrycoder@gmail.com>
2023-08-21 16:48:39 -0400

    Creando servidor http con conexion a Socket

--- nodejs/usandoSockets/src/index.js
+++ nodejs/usandoSockets/src/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
+
 const path = require('path')
 const socketio = require('socket.io')
+const http = require('http')
 const app = express()
 
 app.set('port', 3000)
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(app.get('port'), () => {
+const server = http.createServer(app)
+const io = socketio(server)
+
+require('./socket')(io)
+
+server.listen(app.get('port'), () => {
 
     console.log(`Aplicacion corriendo en el puerto ${app.get('port')}`)
 })
---
+++ nodejs/usandoSockets/src/socket.js
@@ -0,0 +1,4 @@
+module.exports = (io) => {
+
+    console.log('El archivo socket.js se ha cargado correctamente')
+}
