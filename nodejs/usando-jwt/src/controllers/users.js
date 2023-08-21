cfb89d77d95d380a3c80948cb2d1d28fbed62d2c
jeffrycoder <jeffrycoder@gmail.com>
2023-08-21 15:18:12 -0400

    Proyecto funcional antes de crear tokens

--- nodejs/usando-jwt/src/controllers/users.js
+++ nodejs/usando-jwt/src/controllers/users.js
@@ -8,22 +8,34 @@ const getAdmin = (req,res) => {
 
     
 }
-const login = (req,res) => {
+const login = async (req,res) => {
 
+    try {
+        const result = await User.findOne({ email: req.body.email });
+        if (result) {
+          
+
+          if (bcrypt.compareSync(req.body.password, result.password)){
+            res.send('Usuario encontrado. Password correcto');
+          }else{
+            res.send('Usuario encontrado. Password incorrecto');
+          }
+        } 
+        
+        else {
+          res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+      } catch (err) {
+        res.status(500).json({ message: 'Ha ocurrido un error de login' });
+      }
 
 }
+
+// Manera en que se hace actualmente en Mongodb
 const register = async  (req,res) => {
 
     
-
     try{
-
-        // const user = new User({
-        //     name: req.body.name,
-        //     email: req.body.email,
-        //     password: bcrypt.hashSync(req.body.password, 10)
-        // })
-
         const user = await User.create({
             name: req.body.name,
             email: req.body.email,
@@ -39,18 +51,6 @@ const register = async  (req,res) => {
 
         res.status(500).json({message: 'Ha ocurrido un error'})
     }
-
-    // user.save((err, result)=>{
-
-    //     if (err){
-
-    //         res.send('Ha ocurrido un error'+ err)
-
-    //     }else{
-    //         res.send('Usuario registrado correctamente')
-    //     }
-        
-    // })
 
 }
 
