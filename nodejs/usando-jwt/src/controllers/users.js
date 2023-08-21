9ab3c68b82900ed7946b8df68a0b8baaefaf6312
jeffrycoder <jeffrycoder@gmail.com>
2023-08-21 12:50:13 -0400

    Modificando la creacion de usuarios con ayuda de vlady

--- nodejs/usando-jwt/src/controllers/users.js
+++ nodejs/usando-jwt/src/controllers/users.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
-const user = require('../models/users')
+const User = require('../models/users')
 
 
 const getAdmin = (req,res) => {
@@ -12,8 +12,45 @@ const login = (req,res) => {
 
 
 }
-const register = (req,res) => {
+const register = async  (req,res) => {
 
+    
+
+    try{
+
+        // const user = new User({
+        //     name: req.body.name,
+        //     email: req.body.email,
+        //     password: bcrypt.hashSync(req.body.password, 10)
+        // })
+
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10)
+        })
+
+        res.json(user)
+        console.log(user)
+        
+    
+    }catch(err){
+        console.error(err)
+
+        res.status(500).json({message: 'Ha ocurrido un error'})
+    }
+
+    // user.save((err, result)=>{
+
+    //     if (err){
+
+    //         res.send('Ha ocurrido un error'+ err)
+
+    //     }else{
+    //         res.send('Usuario registrado correctamente')
+    //     }
+        
+    // })
 
 }
 
