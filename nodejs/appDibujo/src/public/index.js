b05720cf25f5765ee5c6d1ecf890b25b6938ba1e
jeffrycoder <jeffrycoder@gmail.com>
2023-08-22 12:05:15 -0400

    Agregando coordenadas mas precisas de manera manual

--- nodejs/appDibujo/src/public/index.js
+++ nodejs/appDibujo/src/public/index.js
@@ -4,7 +4,7 @@ var click = false;
 var moving_mouse = false;
 var x_position = 0;
 var y_position = 0;
-var previus_position = {x_position:0,y_position:0};
+var previus_position = null;
 var color  = 'black';
 
 const canvas = document.getElementById('canvas')
@@ -30,9 +30,44 @@ canvas.addEventListener('mouseup', ()=>{
 canvas.addEventListener('mousemove', (event)=>{
 
     
-    x_position = event.clientX
-    y_position = event.clientY
+    x_position = event.clientX - canvas.offsetLeft -8
+    y_position = event.clientY - canvas.offsetTop - 6
     moving_mouse = true;
 
 
 })
+
+
+function create_drawing(){
+
+    if (click && moving_mouse && previus_position != null) {
+
+        let drawing = {
+
+            x_position: x_position,
+            y_position: y_position,
+            color:color,
+            previus_position: previus_position
+        }
+        show_drawing(drawing)
+    
+    }
+
+    previus_position = {x_position:x_position, y_position:y_position}
+    setTimeout(create_drawing, 25)
+
+}
+
+function show_drawing(drawing){
+
+    context.beginPath()
+
+    context.lineWidth = 3
+    context.strokeStyle = drawing.color
+    context.moveTo(drawing.x_position, drawing.y_position)
+    context.lineTo(drawing.previus_position.x_position, drawing.previus_position.y_position)
+    context.stroke()
+
+}
+
+create_drawing()
