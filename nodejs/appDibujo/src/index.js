969785d2d4505630e2fddd98e68adffec05feb03
jeffrycoder <jeffrycoder@gmail.com>
2023-08-22 19:51:10 -0400

    Agregando configuracion para despliegue en un servidor

--- nodejs/appDibujo/src/index.js
+++ nodejs/appDibujo/src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
+const cors = require('cors')
 
 // Para inicializar el app
 const app = express()
@@ -14,10 +15,11 @@ require('./socket')(io)
 
 
 // Para habilitar el puerto
-app.set('port',3000)
+app.set('port',process.env.PORT || 5000)
 
 // Para tener acceso a los archivos estaticos
 app.use(express.static(path.join(__dirname,'public')))
+app.use(cors())
 
 
 server.listen(app.get('port'),()=>{
