385617c4953f0663e208cd1f03f173c572b0b29a
jeffrycoder <jeffrycoder@gmail.com>
2023-08-22 17:09:39 -0400

    Agregando agregando funcionalidad para guardar dibujos

--- nodejs/appDibujo/src/socket.js
+++ nodejs/appDibujo/src/socket.js
@@ -1,11 +1,22 @@
 module.exports = (io) => {
-
     console.log('Socket.io esta listo para usarse')
+
+    var data = []
 
     io.on('connection', (socket) => {
 
+        for (let i = 0; i < data.length; i++) {
+            
+            io.emit('show_drawing', data[i])
+            
+        }
+
+
+
+
         socket.on('drawing', (drawing) => {
 
+            data.push(drawing)
             io.emit('show_drawing', drawing)
 
         })
