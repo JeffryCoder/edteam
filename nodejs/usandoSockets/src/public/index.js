e6d3b41eee084ef4409f859a4961c89612874e06
jeffrycoder <jeffrycoder@gmail.com>
2023-08-22 09:08:57 -0400

    Emitiendo eventos correctamente

--- nodejs/usandoSockets/src/public/index.js
+++ nodejs/usandoSockets/src/public/index.js
@@ -1,1 +1,25 @@
-console.log('Hello World from public/index.js')
+const socket = io()
+
+const username = document.getElementById('username')
+const write_message = document.getElementById('write_message')
+
+
+write_message.addEventListener('keyup', (event) => {
+
+
+    if (event.code == 'Enter') {
+        
+        if (username.value != ''  && write_message.value != '') {
+            
+            socket.emit('message', {
+                username: username.value, 
+                message: write_message.value.slice(0, -1)
+            })
+
+
+        }else{
+            console.log("No se puede enviar el mensaje, verifique los campos")
+        }
+
+    }
+})
--- nodejs/usandoSockets/src/socket.js
+++ nodejs/usandoSockets/src/socket.js
@@ -1,4 +1,24 @@
 module.exports = (io) => {
 
     console.log('El archivo socket.js se ha cargado correctamente')
+
+    io.on('connection', (socket) => {
+        console.log('Un usuario se ha conectado')
+
+
+        socket.on('message', (data) => {
+
+            console.log(data)
+
+        })
+
+
+        socket.on('disconnect', () => {
+
+
+
+            console.log("Un usuario se ha desconectado")
+        })
+    })
+
 }
