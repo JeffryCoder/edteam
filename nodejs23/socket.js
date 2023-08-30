c371c59c2191d961a5098b2a3667eb0f3d34eab5
jeffrycoder <jeffrycoder@gmail.com>
2023-08-30 10:43:08 -0400

    Agregando broadcasting

--- nodejs23/socket.js
+++ nodejs23/socket.js
@@ -3,6 +3,17 @@ module.exports = (io)=> {
     io.on('connection', (socket)=>{
 
         console.log("Se ha conectado un nuevo cliente en el servidor")
+
+        socket.on('add-information', (data)=>{
+
+            console.log(data.info)
+
+
+            io.emit('show-information', data)
+        })
+
+        socket.broadcast.emit('new-connection', 'Nuevo cliente conectado')
+        
         socket.on('disconnect', ()=>{
 
 
