5d35ffde4daa6dd3c5baedd7f94103902ab94f1b
jeffrycoder <jeffrycoder@gmail.com>
2023-09-04 17:28:42 -0400

    Agregando Schema de peliculas para la base de datos

---
+++ creacionApiRestNode/node-express-api/src/models/Movies.js
@@ -0,0 +1,21 @@
+const {Schema, Model} = require("mongoose");
+
+const moviesSchema = new Schema({
+    title: String,
+    year: Number,
+    rate: String,
+    director: String
+
+
+
+},
+{
+    timestamps: true,
+    versionKey: false
+}
+
+
+);    
+
+
+module.exports = model("Movies", moviesSchema)
