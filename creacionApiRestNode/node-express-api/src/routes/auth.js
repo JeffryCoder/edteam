716d107975c9f016e0d0eb7a0286ae04f4d6e2ad
jeffrycoder <jeffrycoder@gmail.com>
2023-09-04 16:21:28 -0400

    Agregando jwt para generar el token

--- creacionApiRestNode/node-express-api/src/routes/auth.js
+++ creacionApiRestNode/node-express-api/src/routes/auth.js
@@ -1,12 +1,32 @@
 const { Router } = require("express");
+const jwt = require("jsonwebtoken");
 const router = Router();
-
+const secretKey = process.env.SECRET_KEY || "secretKey";
 router.post("/api/login", (req, res) => {
     
     const { email, password } = req.body;
-    console.log(email, password);
 
-    res.json({"msg" : "Logueando usuario"});
+    const user = {
+        id: 1,
+        name: "Jeffry",
+        email: "jeffry@frixzito.com",
+        password: "test"
+    };
+
+    if (email === user.email && password === user.password) {
+
+        jwt.sign({user:user}, secretKey, (err, token) => {
+
+
+            res.json({token:token});
+        });
+
+    }else{
+
+        res.json({mensaje: "Usuario o contraseña incorrectos"})
+    }
+
+    
 });
 
 
