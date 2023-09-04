ebbeb67877d9befd947481d00b1b9500eb875fb6
jeffrycoder <jeffrycoder@gmail.com>
2023-09-04 17:58:09 -0400

    Agregando metodo get de todas las peliculas registradas

--- creacionApiRestNode/node-express-api/src/routes/movies.js
+++ creacionApiRestNode/node-express-api/src/routes/movies.js
@@ -3,21 +3,35 @@ const Movies = require("../models/movies");
 
 const router = Router();
 
+router.get("/api/movies", async (req, res) => {
+
+
+    res.json(await Movies.find());
+
+
+
+});
+
 
 router.post("/api/movies", async (req, res) => {
 
-
-    const {title, year, rate, director} = req.body;
+    try {
+        const {title, year, rate, director} = req.body;
     
-    const newMovies = await Movies({
-        title: title,
-        year: year,
-        rate: rate,
-        director: req.body.director // Otra forma de hacerlo
-    })
+        const newMovies = await Movies({
+            title: title,
+            year: year,
+            rate: rate,
+            director: req.body.director // Otra forma de hacerlo
+        })
 
-    newMovies.save();
-    res.json({"mensaje":"Accediendo a peliculas por metodo post"});
+        newMovies.save();
+        res.json({"mensaje":"Pelicula guardada con exito"});
+    } catch (error) {
+        res.status(500).json({"mensaje":"Error al acceder a peliculas por metodo post"});
+    }
+
+    
 
 
 });
