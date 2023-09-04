2d964ec9d03954f586a04ff56f6bbe50c5728083
jeffrycoder <jeffrycoder@gmail.com>
2023-09-04 14:42:04 -0400

    Creando primer endpoint para recibir peticiones

--- creacionApiRestNode/node-express-api/src/index.js
+++ creacionApiRestNode/node-express-api/src/index.js
@@ -1,12 +1,15 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require("express"); // Para importar capacidades de servidor
+const morgan = require("morgan"); // Para ver las peticiones en consola
 
 
-const app = express();
+const app = express(); // Para crear el servidor
 
 // CONSTANTES GLOBALES
-app.set("port", process.env.PORT || 3000)
-app.use(morgan("dev"));
+app.set("port", process.env.PORT || 3000) // Para que el puerto sea el que nos de el servidor o el 3000 si no nos da ninguno
+
+// CONFIGURACIONES
+app.use(morgan("dev")); // Para que muestre las peticiones en consola
+app.use(express.json()); // Para que el servidor entienda los formatos json
 
 //ENDPOINTS
 
@@ -16,6 +19,13 @@ app.get("/", (req, res) => {
 
 });
 
+app.post("/users", (req, res) => {
+
+
+    console.log(req.body)
+    res.json({"mensaje":"Accediendo a usuarios por metodo post"});
+
+});
 
 
 
