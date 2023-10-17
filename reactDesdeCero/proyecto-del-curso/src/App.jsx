66ae566aed4b5799319ee309704659a0436fc1ec
jeffrycoder <jeffrycoder@gmail.com>
2023-10-17 12:14:30 -0400

    Instalando tailwindcss

--- reactDesdeCero/proyecto-del-curso/src/App.jsx
+++ reactDesdeCero/proyecto-del-curso/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import TarjetaCriptomoneda from "./components/TarjetaCriptomoneda"
+import './App.css'
 function App() {
 
   const API_URL = import.meta.env.VITE_API_URL
