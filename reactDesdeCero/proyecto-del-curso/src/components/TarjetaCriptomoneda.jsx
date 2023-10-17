ec240ef5f31d5cbb2a6148f87d64e1b4ce3298b9
jeffrycoder <jeffrycoder@gmail.com>
2023-10-17 16:33:02 -0400

    Eliminando el bg-whiete que causaba problemas

--- reactDesdeCero/proyecto-del-curso/src/components/TarjetaCriptomoneda.jsx
+++ reactDesdeCero/proyecto-del-curso/src/components/TarjetaCriptomoneda.jsx
@@ -19,7 +19,7 @@ function TarjetaCriptomoneda ({id, nombreCriptomoneda = 'NombreCripto', precioCr
   return (
     <>
       <div className="car" id={id}>
-        <a ref={refTarjetaCriptomoneda} href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+        <a ref={refTarjetaCriptomoneda} href="#" className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{nombreCriptomoneda}</h5>
           <p className="font-normal text-gray-700">USD${precioCriptomoneda}</p>
         </a>
