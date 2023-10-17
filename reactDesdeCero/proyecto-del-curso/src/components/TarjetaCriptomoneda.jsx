28f4394720a4f770eada7487af011787aeb91c49
jeffrycoder <jeffrycoder@gmail.com>
2023-10-17 12:16:44 -0400

    Configurando estilos tailwindcss

--- reactDesdeCero/proyecto-del-curso/src/components/TarjetaCriptomoneda.jsx
+++ reactDesdeCero/proyecto-del-curso/src/components/TarjetaCriptomoneda.jsx
@@ -2,8 +2,12 @@ function TarjetaCriptomoneda () {
   return (
     <>
       <div className="card">
-        <span>Nombre Criptomoneda</span>
-        <span>Precio criptomoneda</span>
+        
+      <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Nombre Criptomoneda</h5>
+        <p className="font-normal text-gray-700">Precio criptomoneda</p>
+      </a>
+
       </div>
     </>
   )
