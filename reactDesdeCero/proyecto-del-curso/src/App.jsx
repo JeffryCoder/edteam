7624014e2688683c9602ff64aeb289813514d057
jeffrycoder <jeffrycoder@gmail.com>
2023-10-17 16:22:55 -0400

    Agregando nombres de clases

--- reactDesdeCero/proyecto-del-curso/src/App.jsx
+++ reactDesdeCero/proyecto-del-curso/src/App.jsx
@@ -21,8 +21,8 @@ function App() {
     return (
         
         
-        <div class="flex items-center justify-center w-56 h-56 border border-gray-200 rounded-lg bg-gray-50">
-            <div class="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse">Cargando...</div>
+        <div className="flex items-center justify-center w-56 h-56 border border-gray-200 rounded-lg bg-gray-50">
+            <div className="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse">Cargando...</div>
         </div>
 
       );
