cc21a36e9f779d90848561b8264f07d657cc0a52
jeffrycoder <jeffrycoder@gmail.com>
2023-10-17 11:53:05 -0400

    Agregando listado de criptos

--- reactDesdeCero/proyecto-del-curso/src/App.jsx
+++ reactDesdeCero/proyecto-del-curso/src/App.jsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react"
 function App() {
+
+  const API_URL = import.meta.env.VITE_API_URL
+  const [criptomonedas, setCriptomonedas] = useState()
+
+  useEffect(() => {
+    fetch(`${API_URL}assets`)
+      .then((resp) => resp.json())
+      .then((data) => { 
+        setCriptomonedas(data.data) 
+      })
+      .catch(() => {
+        console.log("La petición falló")
+      })
+  }, [])
+
+  if (!criptomonedas) return <h1>Cargando...</h1>;
 
   return (
     <>
-      <h1>Hola</h1>
+      <h1>Lista de Criptomonedas</h1>
+      <ol>
+        {criptomonedas.map((criptomoneda) => {
+          return <li key={criptomoneda.id}>Nombre: <br /> {criptomoneda.name} <br /> Precio: <br />{criptomoneda.priceUsd}</li>;
+        })}
+      </ol>
     </>
-  )
+  );
+
 }
 
 export default App
