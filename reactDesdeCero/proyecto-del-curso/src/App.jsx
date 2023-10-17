14f441aef843a917e781fbc7db259a4d829bd2dd
jeffrycoder <jeffrycoder@gmail.com>
2023-10-17 16:22:20 -0400

    Agregando loading

--- reactDesdeCero/proyecto-del-curso/src/App.jsx
+++ reactDesdeCero/proyecto-del-curso/src/App.jsx
@@ -17,7 +17,16 @@ function App() {
       })
   }, [])
 
-  if (!criptomonedas) return <h1>Cargando...</h1>;
+  if (!criptomonedas) {
+    return (
+        
+        
+        <div class="flex items-center justify-center w-56 h-56 border border-gray-200 rounded-lg bg-gray-50">
+            <div class="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse">Cargando...</div>
+        </div>
+
+      );
+  }
 
   
   return (
