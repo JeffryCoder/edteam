8b46ba788655ac64514db1077e82f53176513ed6
jeffrycoder <jeffrycoder@gmail.com>
2023-10-17 12:19:19 -0400

    Colocando texto por defecto

--- reactDesdeCero/proyecto-del-curso/src/components/TarjetaCriptomoneda.jsx
+++ reactDesdeCero/proyecto-del-curso/src/components/TarjetaCriptomoneda.jsx
@@ -1,11 +1,11 @@
-function TarjetaCriptomoneda () {
+function TarjetaCriptomoneda ({nombreCriptomoneda = 'NombreCripto', precioCriptomoneda = 'PrecioCripto'}) {
   return (
     <>
       <div className="card">
         
       <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Nombre Criptomoneda</h5>
-        <p className="font-normal text-gray-700">Precio criptomoneda</p>
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{nombreCriptomoneda}</h5>
+        <p className="font-normal text-gray-700">USD${precioCriptomoneda}</p>
       </a>
 
       </div>
