7d12303f407949b9ffff64ddcbec8df534ad98bb
jeffrycoder <jeffrycoder@gmail.com>
2023-10-23 12:50:00 -0400

    Agregando mi copyright

--- reactDesdeCero/proyecto-del-curso/src/App.jsx
+++ reactDesdeCero/proyecto-del-curso/src/App.jsx
@@ -41,7 +41,9 @@ function App() {
           })
         }
       </div>
-        
+        <footer>
+          <p className="text-center m-3"> &copy; Hecho con la <span className="text-red-500">🍆</span> de <a href="https://github.com/jeffrycoder" target="_blank" rel="noreferrer">JeffryCoder</a></p>
+        </footer>
     </div>
   );
 
