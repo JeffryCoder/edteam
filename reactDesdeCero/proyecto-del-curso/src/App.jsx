fc6439b789be004bc7d238838d10d79d61f6916b
jeffrycoder <jeffrycoder@gmail.com>
2023-10-23 12:53:20 -0400

    Ajustando tamano de pantalla de carga

--- reactDesdeCero/proyecto-del-curso/src/App.jsx
+++ reactDesdeCero/proyecto-del-curso/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     return (
         
         
-        <div className="flex items-center justify-center w-56 h-56 border border-gray-200 rounded-lg bg-gray-50">
+        <div className="flex items-center justify-center w-full h-screen border border-gray-200 rounded-lg bg-gray-50">
             <div className="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse">Cargando...</div>
         </div>
 
