d5e57a3b0b4cd344a11641a4e14cd0c0f4cac190
jeffrycoder <jeffrycoder@gmail.com>
2023-10-23 15:10:41 -0400

    Dejando solo el loader solo

--- reactDesdeCero/proyecto-del-curso/src/App.jsx
+++ reactDesdeCero/proyecto-del-curso/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         
         <>
           <div className="loaderContainer flex items-center justify-center w-full h-screen border border-gray-200 rounded-lg bg-gray-50">
-            <div className="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse">Cargando...</div>
+            {/* <div className="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse">Cargando...</div> */}
             <span className="loader"></span>
           </div>
         </>
