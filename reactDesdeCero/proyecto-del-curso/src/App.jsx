f60bef3423c2f6972816546e069d72adf32b7a56
jeffrycoder <jeffrycoder@gmail.com>
2023-10-23 13:03:22 -0400

    Agregando diseno

--- reactDesdeCero/proyecto-del-curso/src/App.jsx
+++ reactDesdeCero/proyecto-del-curso/src/App.jsx
@@ -21,10 +21,13 @@ function App() {
   if (!criptomonedas) {
     return (
         
-        
-        <div className="flex items-center justify-center w-full h-screen border border-gray-200 rounded-lg bg-gray-50">
+        <>
+          <div className="loaderContainer flex items-center justify-center w-full h-screen border border-gray-200 rounded-lg bg-gray-50">
             <div className="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse">Cargando...</div>
-        </div>
+            <span className="loader"></span>
+          </div>
+        </>
+        
 
       );
   }
