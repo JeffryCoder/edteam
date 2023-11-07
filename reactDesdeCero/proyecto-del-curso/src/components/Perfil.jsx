9ddc493510b2bfc1f7db463dc68336e2f7aef999
jeffrycoder <jeffrycoder@gmail.com>
2023-11-07 15:31:00 -0400

    Integrando nueva pestana de perfil

---
+++ reactDesdeCero/proyecto-del-curso/src/components/Perfil.jsx
@@ -0,0 +1,17 @@
+const Perfil = () => {
+  return (
+
+    <div>
+      <h1>Perfi de Jefffry</h1>
+      <p>Bienvenido, Jeffry Sanchez</p>
+      <div>
+        <p>Nombre: Jeffry Sanchez</p>
+        <p>Edad: 21</p>
+        <p>Correo:</p>
+        <p>Usuario desde: 22/06/2022</p>
+      </div>
+    </div>
+  )
+}
+
+export default Perfil
--- reactDesdeCero/proyecto-del-curso/src/main.jsx
+++ reactDesdeCero/proyecto-del-curso/src/main.jsx
@@ -6,11 +6,15 @@ import Pagina404 from './components/Pagina404.jsx'
 import PlantillaRoot from './components/PlantillaRoot.jsx'
 import Home from './components/Home.jsx'
 import PaginaUnicaCriptomoneda from './components/PaginaUnicaCriptomoneda.jsx'
+import Perfil from './components/Perfil.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />}>
         <Route index element={<Home />} />
+      </Route>
+      <Route path='perfil' element={<PlantillaRoot />}>
+        <Route index element={<Perfil />} />
       </Route>
       <Route path='criptomonedas' element={<PlantillaRoot />}>
         <Route index element={<App />} />
