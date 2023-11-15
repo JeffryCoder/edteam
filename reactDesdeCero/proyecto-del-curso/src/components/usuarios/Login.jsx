a7a1b64ac49b35309584f70eef5ca7bfee9ad205
jeffrycoder <jeffrycoder@gmail.com>
2023-11-15 16:33:26 -0400

    Agregando useState

--- reactDesdeCero/proyecto-del-curso/src/components/usuarios/Login.jsx
+++ reactDesdeCero/proyecto-del-curso/src/components/usuarios/Login.jsx
@@ -1,18 +1,46 @@
+import { useState } from 'react'
+
 const Login = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  })
+
+  const submit = (e) => {
+    e.preventDefault()
+    console.log(user)
+  }
   return (
+
     <div className='login-container'>
       <h1>Iniciar sesion</h1>
-      <form>
+      <form onSubmit={submit}>
         <div className='field'>
           <label htmlFor='email'>Correo electronico</label>
-          <input type='email' name='email' />
+          <input
+            required
+            onChange={(e) => {
+              setUser({
+                ...user,
+                email: e.target.value
+              })
+            }} type='email' name='email'
+          />
         </div>
         <div className='field'>
           <label htmlFor='password'>Contraseña</label>
-          <input type='password' name='password' />
+          <input
+            required
+            onChange={(e) => {
+              setUser({
+                ...user,
+                password: e.target.value
+              })
+            }} type='password' name='password'
+          />
         </div>
         <div>
-          <button type='submit'>Iniciar sesion</button>
+          <button type='submit'>Acceder</button>
         </div>
       </form>
     </div>
